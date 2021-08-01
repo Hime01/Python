5bd86552b948d9a15120017c578ec4df794034dd
Lebedeva Yulia <yvlebe24@mts.ru>
2021-08-01 14:21:16 +0300

    [fix] reformat

--- task4.py
+++ task4.py
@@ -3,7 +3,7 @@ user_numb = int(input('Введите число, а я выдам самую б
 divider = 10
 numb = user_numb % divider
 while user_numb / divider > 0:
-    user_numb = int(user_numb/divider)
+    user_numb = int(user_numb / divider)
     next_numb = int(user_numb % divider)
     if numb < next_numb:
         numb = next_numb
--- task5.py
+++ task5.py
@@ -9,10 +9,10 @@ elif diff == 0:
     print('Издержки и выручка равны')
 else:
     print(f'Фирма отработала с прибылью в размере {diff}')
-    rentable = int(earnings/expenses)
+    rentable = int(earnings / expenses)
     print(f'Рентабельность выручки: {rentable}')
     numb_workers = int(input('Сколько сотрудников работает в фирме? '))
-    earnings_worker = int(diff/numb_workers)
+    earnings_worker = int(diff / numb_workers)
     print(f'Прибыль фирмы в расчете на одного сотрудника: {earnings_worker}')
 
 # округляю значения, потому что слышала на вебинаре, что просят делать именно так
