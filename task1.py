a1dd8f5bcea67cdae4b596c968657097650b4439
Lebedeva Yulia <yvlebe24@mts.ru>
2021-08-06 01:10:28 +0300

    [fix] task3

--- task1.py
+++ task1.py
@@ -1,4 +1,8 @@
-my_list = [51, 1.5, 4-1j, 'Capybara', [2, '123123', None], (2, '123123', None), {2, '123123', None}, {'first': 1, 'second': 2}, False, None]
+my_list = [
+    51, 1.5, 4 - 1j, 'Capybara', [2, '123123', None],
+    (2, '123123', None), {2, '123123', None}, {'first': 1, 'second': 2},
+    False, b'text', None, ValueError
+]
 
-for i in my_list:
-    print(type(i))
+for numb, item in enumerate(my_list, 1):
+    print(f'{numb}. {item} - {type(item)}')
