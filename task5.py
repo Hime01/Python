32e6959125fc444b4efb05150af3204ee2485c3a
Lebedeva Yulia <yvlebe24@mts.ru>
2021-08-13 18:05:22 +0300

    [fix] task6

--- task5.py
+++ task5.py
@@ -5,7 +5,7 @@ print(my_list)
 
 
 def reducer_sum(el1, el2):
-    print(f'el1 = {el1}, el2 = {el2}')
-    return el1 + el2
+    #print(f'el1 = {el1}, el2 = {el2}')
+    return el1 * el2
 
 print(reduce(reducer_sum, my_list))
